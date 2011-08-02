7222524e9a122a91da3556b39b3e217d5c49ba88
sept <jfsgj@163.com>
2011-08-02 12:04:15 +0800

    use p and p->next
    in order to insert the new between the two node

--- exercise5/pointer-linklist.c
+++ exercise5/pointer-linklist.c
@@ -17,32 +17,32 @@ STU *create_link(int n)
 	STU *head = NULL;
 	STU *p = NULL;
 
-	head = p = malloc(sizeof(STU));
+	head = p = malloc(sizeof(STU));              //在栈里开辟 空间
 	if(p == NULL)
 	{
-		perror("create");
+		perror("create");                       //如果开辟不成功则会输出“create”
 		exit(0);
 	}
-	p->number = 1;
+	p->number = 1;                              
 	strcpy(p->name, "student");
 	p->next = NULL;
 	
 	for(i=1; i<n; i++)
 	{
-		p->next = malloc(sizeof(STU));
+		p->next = malloc(sizeof(STU));          //在开辟一个结构体类型大小空间后 接着链着开辟相同大小的空间
 		if(p->next == NULL)
 		{
 			perror("create");
 			exit(0);
 		}
-		p->next->number = i+1;
+		p->next->number = i+1;                  //下个结构体里的number 较之前一个加一
 		strcpy(p->next->name, "student");
-		p->next->next = NULL;
-		p = p->next;
+		p->next->next = NULL;                   
+		p = p->next;                            //移动 P 的位置 到下一个
 
 	}
 
-	return head;
+	return head;                                //返回head 固定链表位置
 }
 
 void print_link(STU *p)
@@ -64,12 +64,14 @@ int get_num(STU *p)                    //数出链表的所有节点数
     }
      return counter;
 }
-
+/*为了判断插入的空间的大小介于两个空间的number，
+即 前面大的和后面小的都要顾及 然后会赋值 使之插入其中
+             故使用p和p->next*/
 STU *add_note(STU *p)
 {
     STU *head = p;
-    STU *p_c = NULL;
-    p_c = malloc(sizeof(STU));
+    STU *p_c = NULL;                    
+    p_c = malloc(sizeof(STU));           //出现新的 p_c 结构体类型空间
     if(p_c == NULL)
     {
         perror("create\n");
@@ -86,7 +88,7 @@ STU *add_note(STU *p)
         p_c->next = p;           
         return p_c;                   // 相当于 head = p_c;    return head;
     }
-    while (p->next != NULL)
+    while (p->next != NULL)            //考虑 p_c 在中间及后面的情况
     {
         if (p_c->number < p->next->number)
         {
